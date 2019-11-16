effecda2aa8bdda643e24088492385f2c4450ae4
R3SWebDevelopment <ac25d43d0a5194a754f4884e770ab1f1025467e9>
2019-11-16 10:47:43 -0600

    New relation

--- django/project/my_web/my_web/personas/models.py
+++ django/project/my_web/my_web/personas/models.py
@@ -11,6 +11,10 @@ class Persona(models.Model):
     apellido_paterno = models.CharField(max_length=200, null=False, blank=False)
     fecha_nacimiento = models.DateTimeField(null=False, blank=False)
     genero = models.CharField(max_length=1, null=False, blank=False, choices=GENERO_CHOICES)
+    padre = models.ForeignKey('Persona', null=True, blank=False, related_name='hijos_del_padre', on_delete=models.CASCADE)
+    madre = models.ForeignKey('Persona', null=True, blank=False, related_name='hijos_de_la_madre', on_delete=models.CASCADE)
+    hermanos = models.ManyToManyField('Persona', related_name='mis_hermanos')
+    pareja = models.OneToOneField('Persona', null=True, blank=False, on_delete=models.CASCADE, related_name='mi_pareja')
 
     def __str__(self):
         return "{} {}".format(self.nombre, self.apellido_paterno)

cbfcf09ba947e640a7c95a0e2008668b26481e18
R3SWebDevelopment <ac25d43d0a5194a754f4884e770ab1f1025467e9>
2019-11-21 20:58:38 -0600

    Improve Forms

--- django/project/my_web/my_web/personas/admin.py
+++ django/project/my_web/my_web/personas/admin.py
@@ -1,10 +1,103 @@
 from django.contrib import admin
 
-from .models import Persona
+from .models import (
+    Persona,
+    Hermandad,
+    Miembro,
+)
+from .forms import (
+    PersonaForm,
+    HermandadForm,
+    HermandadFormSet,
+    MiembroForm,
+)
+
+
+class HermanoTabularAdmin(admin.TabularInline):
+    model = Hermandad
+    extra = 0
+    form = HermandadForm
+    formset = HermandadFormSet
+    fk_name = 'persona_uno'
+
+
+class PersonaTabularAdmin(admin.TabularInline):
+    model = Persona
+    extra = 0
+    max_num = 1
+    form = PersonaForm
+
+
+class PadreTabularAdmin(PersonaTabularAdmin):
+    fk_name = 'padre'
+    verbose_name = 'Padre'
+    verbose_name_plural = "Padre"
+
+
+class MadreTabularAdmin(PersonaTabularAdmin):
+    fk_name = 'madre'
+    verbose_name = 'Madre'
+    verbose_name_plural = "Madre"
+
+
+class ParejaTabularAdmin(PersonaTabularAdmin):
+    fk_name = 'pareja'
+    verbose_name = 'Pareja'
+    verbose_name_plural = "Pareja"
+
+
+def enroll_persona_membresia(modeladmin, request, queryset):
+    for persona in queryset:
+        if not persona.is_member:
+            Miembro.enroll_persona(persona=persona)
+
+
+enroll_persona_membresia.short_description = "Inscribir Miembro"
 
 
 class PersonaAdmin(admin.ModelAdmin):
-    pass
+    list_display = [  # Aqui definimos los campos que se muestran en el listado de personas
+        'nombre', 'apellido_paterno', 'genero', 'tiene_pareja', 'tiene_padre', 'tiene_madre', 'tiene_hermanos',
+        'es_miembro',
+    ]
+    search_fields = [  # Aqui definimos los campos que se utilizan para hacer busquedas
+        "nombre", 'apellido_paterno',
+    ]
+    list_filter = [  # Aqui definimos los campos que se utilizan para hacer filtros
+        'apellido_paterno', 'genero',
+    ]
+    form = MiembroForm
+
+    inlines = [
+        PadreTabularAdmin,
+        MadreTabularAdmin,
+        ParejaTabularAdmin,
+        HermanoTabularAdmin,
+    ]
+    actions = [enroll_persona_membresia]
+
+    def tiene_pareja(self, obj):
+        return obj.tiene_pareja
+
+    def tiene_padre(self, obj):
+        return obj.tiene_padre
+
+    def tiene_madre(self, obj):
+        return obj.tiene_madre
+
+    def tiene_hermanos(self, obj):
+        return obj.tiene_hermanos
+
+    def es_miembro(self, obj):
+        return obj.is_member
 
 
 admin.site.register(Persona, PersonaAdmin)
+
+
+class MiembroAdmin(PersonaAdmin):
+    list_display = PersonaAdmin.list_display
+    list_filter = PersonaAdmin.list_filter
+
+
+admin.site.register(Miembro, MiembroAdmin)
---
+++ django/project/my_web/my_web/personas/forms.py
@@ -0,0 +1,76 @@
+from django import forms
+
+from .models import (
+    Persona,
+    Hermandad,
+    GENERO_CHOICES,
+    Miembro,
+)
+
+
+class PersonaForm(forms.ModelForm):
+
+    class Meta:
+        models = Persona
+        # Definimos los campos que se van a mostrar en la forma
+        fields = ['nombre', 'apellido_paterno', 'genero']
+
+    def __init__(self, *args, **kwargs):
+        # Es necesario inicializar la clase padre ModelForm
+        super(PersonaForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:  # Usando self.fields accedemos a los campos que se estan desplegando
+            # la variable field, es una llave del diccionario de fields
+            if field in ['pareja', 'padre', 'madre']:
+                # Si el valor de field coincide, define el campo como no requerido
+                self.fields[field].required = False
+
+
+class MiembroForm(PersonaForm):
+
+    class Meta:
+        models = Miembro
+        # Definimos los campos que se van a mostrar en la forma
+        fields = PersonaForm.Meta.fields + ['numero_membresia']
+
+
+class HermandadFormSet(forms.BaseInlineFormSet):
+
+    def get_form_kwargs(self, index):
+        kwargs = super(HermandadFormSet, self).get_form_kwargs(index)
+        kwargs.update({'parent': self.instance})
+        return kwargs
+
+
+class HermandadForm(forms.ModelForm):
+    nombre = forms.CharField(max_length=200, label='Nombre')
+    apellido_paterno = forms.CharField(max_length=200, label='Apellido Paterno')
+    genero = forms.ChoiceField(choices=GENERO_CHOICES, label='Genero')
+
+    class Meta:
+        models = Hermandad
+        # Definimos los campos que se van a mostrar en la forma
+        fields = ['persona_dos', 'nombre', 'apellido_paterno', 'genero']
+
+    def __init__(self, *args, **kwargs):
+        self.parent = kwargs.get('parent', None)
+        if self.parent is not None:
+            del kwargs['parent']
+        # Es necesario inicializar la clase padre ModelForm
+        super(HermandadForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].required = False
+        if self.parent is not None:
+            self.fields['persona_dos'].queryset = Persona.objects.exclude(id=self.parent.id).all()
+            self.fields['persona_dos'].label = 'Seleccionar Persona'
+
+        if self.instance is not None and len(str(self.instance).strip()) > 0:
+            # La forma esta relacionada a una persona
+
+            # Inicializar los campos nombre, apellido_paterno y genero basado en el hermano (persona) seleccionada
+            self.fields['nombre'].initial = self.instance.persona_dos.nombre
+            self.fields['apellido_paterno'].initial = self.instance.persona_dos.apellido_paterno
+            self.fields['genero'].initial = self.instance.persona_dos.genero
+        else:
+            # La forma no esta relacionada a ninguna persona
+            pass

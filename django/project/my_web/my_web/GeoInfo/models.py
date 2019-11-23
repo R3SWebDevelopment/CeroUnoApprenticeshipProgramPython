b6f96ed0c2eab12b8fc3b43b00f9c438ba4dbc4c
R3SWebDevelopment <ac25d43d0a5194a754f4884e770ab1f1025467e9>
2019-11-23 14:30:03 -0600

    Add GeoInfo App and improve Setting_UP file

---
+++ django/project/my_web/my_web/GeoInfo/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+
+class Estado(models.Model):
+    nombre = models.CharField(max_length=200, null=False, blank=False)
+    pais = models.ForeignKey(Pais, null=False, blank=False, related_name='estados', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(
+            self.pais,
+            self.nombre
+        )
+
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=200, null=False, blank=False)
+    estado = models.ForeignKey(Estado, null=False, blank=False, related_name='estados', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(
+            self.pais,
+            self.nombre
+        )

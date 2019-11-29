c188fb7f60e591ce4cf8d35b26a219ff839801bb
R3SWebDevelopment <ac25d43d0a5194a754f4884e770ab1f1025467e9>
2019-11-29 16:39:23 -0600

    Add statics dir settings

--- django/project/my_web/my_web/settings.py
+++ django/project/my_web/my_web/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_raw"),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 FIXTURE_DIRS = os.path.join(BASE_DIR, 'fixtures')
